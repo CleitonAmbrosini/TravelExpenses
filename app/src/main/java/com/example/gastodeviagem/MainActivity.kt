d13f4f0878a5a867c78e88ff96a9dbd1f01ed699
Cleiton Ambrosini <cleiton.ambro@gmail.com>
2021-09-26 18:22:11 -0300

    feat(MainForm): added feedback to the user
    added a Toast for when any field is empty or some value is equal to 0

--- app/src/main/java/com/example/gastodeviagem/MainActivity.kt
+++ app/src/main/java/com/example/gastodeviagem/MainActivity.kt
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Toast
+import java.lang.NumberFormatException
 
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    var message: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,16 +27,41 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun calculate() {
-        if (isValidForm()) {
-            val distance = editDistance.text.toString().toFloat()
-            val price = editPrice.text.toString().toFloat()
-            val autonomy = editAutonomy.text.toString().toFloat()
-            val totalValue = (distance * price) / autonomy
-            textTotalValue.text = "R$ ${"%.2f".format(totalValue)}"
+        if (isValidForm() && !thereAreAZeroValue()) {
+            try {
+                val distance = editDistance.text.toString().toFloat()
+                val price = editPrice.text.toString().toFloat()
+                val autonomy = editAutonomy.text.toString().toFloat()
+                val totalValue = (distance * price) / autonomy
+                textTotalValue.text = "R$ ${"%.2f".format(totalValue)}"
+            } catch (nfe: NumberFormatException) {
+                Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+            }
+        } else {
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show()
         }
     }
 
-    private fun isValidForm(): Boolean = (editDistance.text.toString() != ""
+    private fun isValidForm(): Boolean {
+        if(editDistance.text.toString() != ""
             && editPrice.text.toString() != ""
-            && editAutonomy.text.toString() != "")
+            && editAutonomy.text.toString() != "") {
+            return true
+        }
+        message = getString(R.string.preencha_todos_campos)
+        return false
+    }
+
+    private fun thereAreAZeroValue(): Boolean {
+        if(editDistance.text.toString() == "0"
+            || editPrice.text.toString() == "0"
+            || editAutonomy.text.toString() == "0") {
+            message = getString(R.string.informe_valores_validos)
+            return true
+        }
+        return false
+    }
+
+
+
 }
