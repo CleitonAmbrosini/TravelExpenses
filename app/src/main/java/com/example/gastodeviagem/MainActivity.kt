6227e7191021219d29686752d98c2e2213c4e9ca
Cleiton Ambrosini <cleiton.ambro@gmail.com>
2021-09-26 17:46:48 -0300

    feat(MainForm): added onClick function in "Calcular" button
    added function that calculates the total travel expenses and validations for blank values in the form fields

--- app/src/main/java/com/example/gastodeviagem/MainActivity.kt
+++ app/src/main/java/com/example/gastodeviagem/MainActivity.kt
@@ -2,11 +2,37 @@ package com.example.gastodeviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonCalculate.setOnClickListener(this)
     }
+
+    override fun onClick(view: View) {
+        val id = view.id
+        if (id == R.id.buttonCalculate) {
+            calculate()
+        }
+    }
+
+    private fun calculate() {
+        if (isValidForm()) {
+            val distance = editDistance.text.toString().toFloat()
+            val price = editPrice.text.toString().toFloat()
+            val autonomy = editAutonomy.text.toString().toFloat()
+            val totalValue = (distance * price) / autonomy
+            textTotalValue.text = "R$ ${"%.2f".format(totalValue)}"
+        }
+    }
+
+    private fun isValidForm(): Boolean = (editDistance.text.toString() != ""
+            && editPrice.text.toString() != ""
+            && editAutonomy.text.toString() != "")
 }
